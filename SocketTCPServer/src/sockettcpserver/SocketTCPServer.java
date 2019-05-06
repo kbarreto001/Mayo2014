822d75cd8b12fa90fb74c634663170b0a1abbf00
kbarreto001 <kbarreto001@ikasle.ehu.eus>
2019-05-06 17:38:35 +0200

    Iniciando

---
+++ SocketTCPServer/src/sockettcpserver/SocketTCPServer.java
@@ -0,0 +1,13 @@
+
+package sockettcpserver;
+
+import static sockettcpserver.ConnectionS.connection;
+
+public class SocketTCPServer {
+
+    public static void main(String[] args) { 
+        System.out.println("Soy el Servidor");
+        connection();
+    }
+    
+}
