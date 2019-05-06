1cc06706b7691b2c35b5379fffc440a120d7def9
781553 <781553@u109071.xa.bi.ehu.es>
2019-05-06 19:57:04 +0200

    ahi voy

--- SocketTCPClient/src/sockettcpclient/ConnectionC.java
+++ SocketTCPClient/src/sockettcpclient/ConnectionC.java
@@ -1,4 +1,3 @@
-
 package sockettcpclient;
 
 import java.io.DataInputStream;
@@ -8,54 +7,65 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.Socket;
 
-
 class ConnectionC {
-    File FICHEROJPG = new File("FileA1.jpg");    
-        public void Connection(String[] nombreyP) {
-            Byte[] fichero;
+
+    File FICHEROJPG = new File("FileA1.jpg");
+
+    public void Connection(String[] nombreyP) {
+        Byte[] fichero;
         try (
                 Socket client = new Socket(nombreyP[0], Integer.parseInt(nombreyP[1]));
-                DataOutputStream dos = new DataOutputStream(client.getOutputStream()); 
+                DataOutputStream dos = new DataOutputStream(client.getOutputStream());
                 DataInputStream dis = new DataInputStream(client.getInputStream());) {
-            
-                System.out.println("fichero: "+FICHEROJPG.length());
-                fichero = VolcarFichero(FICHEROJPG);
-                System.out.println(fichero[60]);
-                
-                enviarFichero(fichero,dos);
+
+            System.out.println("fichero: " + FICHEROJPG.length());
+            fichero = VolcarFichero(FICHEROJPG);
+
+            enviarFichero(fichero, dos, dis);
 
         } catch (IOException e) {
             System.out.println(e);
         }
     }
-        
-    public Byte[] VolcarFichero(File FICHEROJPG){ 
-        int length = (int)FICHEROJPG.length();
-        Byte[] fichero = new Byte[length];        
-        try(
-                DataInputStream dis = new DataInputStream(new FileInputStream(FICHEROJPG));
-                ){
-            for(int i=0;i<fichero.length;i++){
-                fichero[i]=dis.readByte();                
+
+    public Byte[] VolcarFichero(File FICHEROJPG) {
+        int length = (int) FICHEROJPG.length();
+        Byte[] fichero = new Byte[length];
+        try (
+                DataInputStream dis = new DataInputStream(new FileInputStream(FICHEROJPG));) {
+            for (int i = 0; i < fichero.length; i++) {
+                fichero[i] = dis.readByte();
             }
-        } catch (IOException ex){
-            System.out.println("Error: "+ex.getLocalizedMessage());
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex.getLocalizedMessage());
         }
         return fichero;
     }
-    
-    public void enviarFichero(Byte[] fichero,DataOutputStream dos) throws IOException{
-        int bloque;
-        try{
-            for (int i=0;i<35;i++){
-                bloque = (i+1)*1000;
-                for(int j=0;j<bloque;j++){
-                    
+
+    public void enviarFichero(Byte[] fichero, DataOutputStream dos, DataInputStream dis) throws IOException {
+        int lengthFichero = fichero.length;
+        int inicioBloque = 0;
+        try {
+            dos.writeInt(lengthFichero);
+            while (true) {
+                if (inicioBloque < lengthFichero) {
+                    for (int i = inicioBloque; i < inicioBloque + 1000; i++) {
+                        dos.writeByte(fichero[i]);
+                    }
+                    inicioBloque = inicioBloque + 1000;
+                    System.out.println(dis.readUTF());
+                } else {
+                    for (int j = inicioBloque; j < lengthFichero; j++) {
+                        dos.writeByte(fichero[j]);
+                        System.out.println("Aqui");
+                    }
+                    break;
                 }
-                dos.writeByte(fichero[i]);
             }
-        } catch (IOException ex){
-            System.out.println("Error: "+ex.getLocalizedMessage());
+            System.out.println("Todo se ha enviado: " + inicioBloque);
+            System.out.println(dis.readUTF());
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex.getLocalizedMessage());
         }
     }
 }
--- SocketTCPClient/src/sockettcpclient/SocketTCPClient.java
+++ SocketTCPClient/src/sockettcpclient/SocketTCPClient.java
@@ -7,7 +7,7 @@ public class SocketTCPClient {
 
     public static void main(String[] args) {
         String[] nombreyP = new String[2];
-        nombreyP[0] = "ubuntu";
+        nombreyP[0] = "localhost";
         nombreyP[1] = "5000";
         System.out.println("Soy el cliente");
         ConnectionC otro = new ConnectionC();
