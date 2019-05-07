7f7c84b298c3e5be07b3af9292d2a3a2dcc7d64b
kbarreto001 <kbarreto001@ikasle.ehu.eus>
2019-05-07 17:21:17 +0200

    tcp done

--- SocketTCPClient/src/sockettcpclient/ConnectionC.java
+++ SocketTCPClient/src/sockettcpclient/ConnectionC.java
@@ -4,28 +4,34 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.Socket;
+import java.util.Objects;
 
 class ConnectionC {
 
-    File FICHEROJPG = new File("FileA1.jpg");
+    protected File FICHEROJPG = new File("FileA1.jpg");
+    protected File FICHEROJPGDEC = new File("FileA2.jpg");
 
-    public void Connection(String[] nombreyP) {
-        Byte[] fichero;
-        try (
-                Socket client = new Socket(nombreyP[0], Integer.parseInt(nombreyP[1]));
-                DataOutputStream dos = new DataOutputStream(client.getOutputStream());
-                DataInputStream dis = new DataInputStream(client.getInputStream());) {
+    public void Connection(DataOutputStream dosSocket, DataInputStream disSocket) {
+        Byte[] fichero, ficheroDec;
+        fichero = VolcarFichero(FICHEROJPG);
+        try {
+            enviarFichero(fichero, dosSocket, disSocket);
+            ficheroDec = RecibirFichero(disSocket, dosSocket);
+            guardarFichero(FICHEROJPGDEC,ficheroDec);
 
-            System.out.println("fichero: " + FICHEROJPG.length());
-            fichero = VolcarFichero(FICHEROJPG);
-
-            enviarFichero(fichero, dos, dis);
-
-        } catch (IOException e) {
-            System.out.println(e);
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex.getLocalizedMessage());
         }
+    }
+
+    public String[] Inicio() {
+        String[] nombreyP = new String[2];
+        nombreyP[0] = "localhost";
+        nombreyP[1] = "5000";
+        System.out.println("Soy el cliente");
+        return nombreyP;
     }
 
     public Byte[] VolcarFichero(File FICHEROJPG) {
@@ -42,30 +48,74 @@ class ConnectionC {
         return fichero;
     }
 
-    public void enviarFichero(Byte[] fichero, DataOutputStream dos, DataInputStream dis) throws IOException {
+    public void guardarFichero(File FICHEROJPGDEC, Byte[] ficheroDec){
+        try(
+                DataOutputStream dos = new DataOutputStream(new FileOutputStream(FICHEROJPGDEC));
+                ){
+            for(int i=0;i<ficheroDec.length;i++){
+                dos.writeByte(ficheroDec[i]);
+            }            
+        } catch(IOException ex){
+            System.out.println("Error: "+ex.getLocalizedMessage());
+        }
+    }
+
+    public void enviarFichero(Byte[] fichero, DataOutputStream dosSocket, DataInputStream disSocket) throws IOException {
         int lengthFichero = fichero.length;
         int inicioBloque = 0;
+        Byte comprobacion;
         try {
-            dos.writeInt(lengthFichero);
+            dosSocket.writeInt(lengthFichero);
             while (true) {
-                if (inicioBloque < (lengthFichero-1000)) {
+                if (inicioBloque < (lengthFichero - 1000)) {
                     for (int i = inicioBloque; i < inicioBloque + 1000; i++) {
-                        dos.writeByte(fichero[i]);
+                        dosSocket.writeByte(fichero[i]);
                     }
                     inicioBloque = inicioBloque + 1000;
-                    System.out.println(dis.readUTF());
+                    System.out.println(disSocket.readUTF());
                 } else {
                     for (int j = inicioBloque; j < lengthFichero; j++) {
-                        dos.writeByte(fichero[j]);
-                        System.out.println("Aqui");
+                        dosSocket.writeByte(fichero[j]);
                     }
                     break;
                 }
             }
-            System.out.println("Todo se ha enviado: " + inicioBloque);
-            System.out.println(dis.readUTF());
+            if (Objects.equals(comprobacion = disSocket.readByte(), fichero[lengthFichero - 1])) {
+                System.out.println("Todo se ha enviado");
+            } else {
+                System.out.println("No se envio completamente");
+            }
         } catch (IOException ex) {
             System.out.println("Error: " + ex.getLocalizedMessage());
         }
+    }
+
+    public Byte[] RecibirFichero(DataInputStream disSocket, DataOutputStream dosSocket) throws IOException {
+        Byte[] fichero = null;
+        int length, inicioBloque = 0, k = 0;
+        try {
+            length = disSocket.readInt();
+            fichero = new Byte[length];
+            while (true) {
+                if (inicioBloque < (length - 1000)) {
+                    for (int i = inicioBloque; i < inicioBloque + 1000; i++) {
+                        fichero[i] = disSocket.readByte();
+                    }
+                    inicioBloque = inicioBloque + 1000;
+                    dosSocket.writeUTF("Recibidos correctamente: " + inicioBloque);
+                } else {
+                    for (int j = inicioBloque; j < length; j++) {
+                        fichero[j] = disSocket.readByte();
+                        k++;
+                    }
+                    break;
+                }
+            }
+            System.out.println("Todo se ha recibido: " + fichero.length);
+            dosSocket.writeByte(fichero[length - 1]);
+        } catch (IOException ex) {
+            System.out.println("Error: 1" + ex.getLocalizedMessage());
+        }
+        return fichero;
     }
 }
--- SocketTCPClient/src/sockettcpclient/SocketTCPClient.java
+++ SocketTCPClient/src/sockettcpclient/SocketTCPClient.java
@@ -1,17 +1,25 @@
-
 package sockettcpclient;
 
-
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 public class SocketTCPClient {
 
-    public static void main(String[] args) {
-        String[] nombreyP = new String[2];
-        nombreyP[0] = "localhost";
-        nombreyP[1] = "5000";
-        System.out.println("Soy el cliente");
+    public static void main(String[] args) throws IOException {
         ConnectionC otro = new ConnectionC();
-        otro.Connection(nombreyP);
+        String[] nombreyP = new String[2];
+        nombreyP = otro.Inicio();
+        try (
+                Socket client = new Socket(nombreyP[0], Integer.parseInt(nombreyP[1]));
+                DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+                DataInputStream dis = new DataInputStream(client.getInputStream());) {
+            
+            otro.Connection(dos, dis);
+            
+        }
+
     }
-    
+
 }
