431b959a22d25baac351ae20e5b5c0b08738eb61
kbarreto001 <kbarreto001@ikasle.ehu.eus>
2019-05-07 17:23:57 +0200

    tcp done

--- SocketTCPServer/src/sockettcpserver/Algoritmo.java
+++ SocketTCPServer/src/sockettcpserver/Algoritmo.java
@@ -5,7 +5,7 @@ public class Algoritmo {
     public Byte[] Decodificar(Byte[] fichero){
         Byte[] ficheroDec = new Byte[fichero.length];        
         for (int i=0;i<fichero.length;i++){
-            ficheroDec[i] = (byte)(255-fichero[i]);
+            ficheroDec[i] = (byte)(255-(int)fichero[i]);
         }        
         return ficheroDec;
     }
